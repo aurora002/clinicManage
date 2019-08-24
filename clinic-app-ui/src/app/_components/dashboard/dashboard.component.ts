49d7caff8ddbbbb3716368da1b9ab91a5aef5e73
sarah <nawsarah.aye@sicmsb.com>
2019-08-24 16:54:21 +0800

    table for patient list

--- clinic-app-ui/src/app/_components/dashboard/dashboard.component.ts
+++ clinic-app-ui/src/app/_components/dashboard/dashboard.component.ts
@@ -1,9 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { AuthenticationService } from 'src/app/_services/authentication.service';
 import { Route } from '@angular/compiler/src/core';
 import { Router } from '@angular/router';
 import { PatientService } from 'src/app/_services/Patient.service';
 import { Patient } from 'src/app/_models/patient';
+import { SelectionModel } from '@angular/cdk/collections';
+import { MatTableDataSource } from '@angular/material/table';
+import { MatSort } from '@angular/material/sort';
+import {MatPaginator} from '@angular/material/paginator';
 
 @Component({
   selector: 'app-dashboard',
@@ -11,30 +15,67 @@ import { Patient } from 'src/app/_models/patient';
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent implements OnInit {
+  @ViewChild(MatSort, { static: true }) sort: MatSort;
+  @ViewChild(MatPaginator, {static: true}) paginator: MatPaginator;
+  user = '';
+  // dataSource: Patient[];
+  displayedColumns: string[] = [
+    'id',
+    'ic',
+    'name',
+    'age',
+    'gender',
+    'dob',
+    'phoneNumber',
+    'address'
+  ];
+  dataSource = new MatTableDataSource<Patient>();
+  selection = new SelectionModel<Patient>(true, []);
 
-user = '';
-patients: Patient[];
-
-constructor(private authService: AuthenticationService,
-            private router: Router,
-            private patientService: PatientService) { }
-
-ngOnInit() {
-      if (this.authService.isUserLoggedIn()) {
-        this.user = sessionStorage.getItem('email');
-      }
-
-      this.patientService.getAllPatients()
-        .subscribe(
-          data => {
-            
-            this.patients = data;
-            console.log(this.patients);
-          },
-          error => {
-
-          });
+  /** Whether the number of selected elements matches the total number of rows. */
+  isAllSelected() {
+    const numSelected = this.selection.selected.length;
+    const numRows = this.dataSource.data.length;
+    return numSelected === numRows;
   }
+
+  /** Selects all rows if they are not all selected; otherwise clear selection. */
+  masterToggle() {
+    this.isAllSelected() ?
+        this.selection.clear() :
+        this.dataSource.data.forEach(row => this.selection.select(row));
+  }
+
+  /** The label for the checkbox on the passed row */
+  checkboxLabel(row?: Patient): string {
+    if (!row) {
+      return `${this.isAllSelected() ? 'select' : 'deselect'} all`;
+    }
+    return `${this.selection.isSelected(row) ? 'deselect' : 'select'} row ${row.id + 1}`;
+  }
+
+  constructor(
+    private authService: AuthenticationService,
+    private router: Router,
+    private patientService: PatientService
+  ) {}
+
+  ngOnInit() {
+    this.dataSource.sort = this.sort;
+    this.dataSource.paginator = this.paginator;
+
+    
+    if (this.authService.isUserLoggedIn()) {
+      this.user = sessionStorage.getItem('email');
+    }
+    this.patientService.getAllPatients().subscribe(
+      data => {
+        this.dataSource = data;
+        console.log(this.dataSource);
+      },
+      error => {}
+      );
+    }
 
   logout() {
     this.authService.logout();
--- clinic-app-ui/src/app/material.module.ts
+++ clinic-app-ui/src/app/material.module.ts
@@ -1,11 +1,24 @@
 import { NgModule, Component } from '@angular/core';
-import {MatNativeDateModule,MatIconModule,MatButtonModule,MatCheckboxModule, MatToolbarModule, MatCardModule,MatFormFieldModule,MatInputModule,MatRadioModule,MatListModule,} from  '@angular/material';
-import {MatDatepickerModule} from  '@angular/material/datepicker';
+import {
+  MatNativeDateModule,
+  MatIconModule,
+  MatButtonModule,
+  MatCheckboxModule,
+  MatToolbarModule,
+  MatCardModule,
+  MatFormFieldModule,
+  MatInputModule,
+  MatRadioModule,
+  MatListModule
+} from '@angular/material';
+import { MatDatepickerModule } from '@angular/material/datepicker';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import {MatSelectModule} from '@angular/material/select';
- 
+import { MatSelectModule } from '@angular/material/select';
+import { MatTableModule } from '@angular/material/table';
+import {MatPaginatorModule} from '@angular/material/paginator';
+
 @NgModule({
-imports: [
+  imports: [
     MatNativeDateModule,
     MatDatepickerModule,
     MatIconModule,
@@ -19,23 +32,25 @@ imports: [
     MatListModule,
     MatRadioModule,
     MatSelectModule,
-],
-exports: [
+    MatTableModule,
+    MatPaginatorModule
+  ],
+  exports: [
     MatNativeDateModule,
     FormsModule,
     MatDatepickerModule,
     MatIconModule,
     MatButtonModule,
     MatCheckboxModule,
-    MatToolbarModule, 
+    MatToolbarModule,
     MatCardModule,
     MatFormFieldModule,
     MatInputModule,
     MatListModule,
     MatRadioModule,
-    MatSelectModule
-],
- 
+    MatSelectModule,
+    MatTableModule,
+    MatPaginatorModule
+  ]
 })
- 
-export  class  MyMaterialModule { }
+export class MyMaterialModule {}
